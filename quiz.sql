05ebc3e8b1f20b4db7ce88fbf192120bd9115b2d
Ulugbek <ulugbekmavlonov@gmail.com>
2023-04-12 22:09:24 +0500

    lesson exam

--- quiz.sql
+++ quiz.sql
@@ -1,1 +1,21 @@
-hello world
+set search_path = my_schema, "$user",salesordersexample;
+
+-- 1-ex
+--select vendors.vendname, count(product_vendors.productnumber) from vendors
+--inner join product_vendors ON product_vendors.vendorid = vendors.vendorid
+--group by vendors.vendname
+
+--2-ex
+
+--select count(recipeclassid) from recipe_classes where recipeclassdescription='Main course'
+
+--3-ex
+
+--set search_path = my_schema, "$user", bowlingleagueexample;
+--select bowlerlastname  from bowlers b inner join teams t
+--on b.teamid = t.teamid where t.teamid between 3 and 5 and bowlerlastname like 'H%'
+
+--4-ex
+
+--set search_path = my_schema, "$user", recipesexample;
+--select recipetitle,* from recipes where notes is null
